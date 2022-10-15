2654de7d1883ff5f4afd66a11e55cdff0e2c6671
briangor <briangor13@gmail.com>
2022-10-15 17:56:49 +0300

    implement sweetalert

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 let weblink;
 let btn = document.getElementById("genQrBtn");
 
-btn.addEventListener("click", generateQR);
-
-function generateQR() {
+let generateQR = () => {
     weblink = document.getElementById('weblink').value;
 
     if (!weblink) {
-        error = 'The link is empty!';
-        alert(error);
+
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          });
+          
+          Toast.fire({
+            icon: 'warning',
+            title: 'The link is empty!'
+          });
+
         return;
     }
 
     new QRious({
         element: document.getElementById("qrcode"),
-        background: '#ffffff',
+        background: '#fff',
         backgroundAlpha: 1,
         foreground: '#5868bf',
         foregroundAlpha: 1,
         level: 'H',
         padding: 0,
-        size: 256,
+        size: 300,
         value: weblink
       });
 
     console.log('generate QR');
 }
+
+btn.addEventListener("click", generateQR);
