2865d452ce0fea17792222412fc3d6bf622de525
briangor <briangor13@gmail.com>
2022-10-17 21:45:43 +0300

    add dynamic class to input box

--- js/script.js
+++ js/script.js
@@ -1,44 +1,48 @@
 let weblink;
 let btn = document.getElementById("genQrBtn");
+let input_box = document.getElementById('weblink');
 
 let generateQR = () => {
-    weblink = document.getElementById('weblink').value;
+  weblink = document.getElementById('weblink').value;
 
-    if (!weblink) {
+  if (!weblink) {
+    
+    input_box.classList.add('empty-input');
 
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          });
-          
-          Toast.fire({
-            icon: 'warning',
-            title: 'The link is empty!'
-          });
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    });
 
-        return;
-    }
+    Toast.fire({
+      icon: 'warning',
+      title: 'The link is empty!'
+    });
 
-    new QRious({
-        element: document.getElementById("qrcode"),
-        background: '#fff',
-        backgroundAlpha: 1,
-        foreground: '#5868bf',
-        foregroundAlpha: 1,
-        level: 'H',
-        padding: 0,
-        size: 300,
-        value: weblink
-      });
+    return;
+  }
+  input_box.classList.remove('empty-input');
 
-    console.log('generate QR');
+  new QRious({
+    element: document.getElementById("qrcode"),
+    background: '#fff',
+    backgroundAlpha: 1,
+    foreground: '#5868bf',
+    foregroundAlpha: 1,
+    level: 'H',
+    padding: 0,
+    size: 300,
+    value: weblink
+  });
+
+  console.log('generate QR');
 }
 
 btn.addEventListener("click", generateQR);
